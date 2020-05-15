f699b0198d833f008c2ff9f8d3d50930062d02ba
jhar3ll <jharell.chambers@selu.edu>
2020-05-14 22:12:57 -0500

    --done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # Add your routes here
+  get 'welcome/home' => 'welcome#home'
+  root 'welcome#home'
+
+  match '/app/github/callback', to: 'sessions#create', via: [:get, :post]
 end
